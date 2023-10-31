6072d81df583d5146c639a62d756302a4a45eb32
Yesil <yesil@gmail.com>
2023-10-31 18:11:11 +0300

    feat: movies app and model

---
+++ movies/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from .models import Category, Movie, IndexMovie
+
+
+# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ['name', 'slug', 'creation_date', 'update_date']
+    search_fields = ['name']
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ['title', 'releasedate', 'director', 'hours', 'creation_date', 'update_date']
+    search_fields = ['title']
+
+
+@admin.register(IndexMovie)
+class IndexMovieAdmin(admin.ModelAdmin):
+    list_display = ['name', 'sure', 'youtube_link']
+    search_fields = ['name']
+
---
+++ movies/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+from django.utils.text import slugify
+
+
+# Create your models here
+#
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True, null=True)
+    creation_date = models.DateTimeField(auto_now_add=True)  # Oluşturulma tarihi
+    update_date = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=150)
+    slug = models.SlugField(max_length=50, unique=True, null=True)
+    description = models.TextField()
+    category = models.ManyToManyField(Category, related_name='film_category')
+    releasedate = models.DateField()
+    director = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='movie_image')
+    hours = models.DurationField()
+    puan = models.DecimalField(max_digits=3, decimal_places=2, default=0.0)
+    creation_date = models.DateTimeField(auto_now_add=True)  # Oluşturulma tarihi
+    update_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Movie, self).save(*args, **kwargs)
+
+
+class IndexMovie(models.Model):
+    youtube_link = models.URLField()
+    image = models.ImageField(upload_to='movie_image')
+    name = models.CharField(max_length=50)
+    sure = models.FloatField()
+
+    def __str__(self):
+        return self.name
